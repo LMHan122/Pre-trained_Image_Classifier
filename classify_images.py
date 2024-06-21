487c5a84612a3139ad803587848a267e93e0dc19
lhan122 <lhan122@wgu.edu>
2024-06-21 16:16:56 -0500

    In classify_images.py: - removed extra comments - changed for loop for the dict to use items() for clairity - concat filepath using os.path

--- classify_images.py
+++ classify_images.py
@@ -1,34 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
-#                                                                             
-# PROGRAMMER: Leslie Hanson
-# DATE CREATED:  2024-06-20                               
-# REVISED DATE: 
-# PURPOSE: Create a function classify_images that uses the classifier function 
-#          to create the classifier labels and then compares the classifier 
-#          labels to the pet image labels. This function inputs:
-#            -The Image Folder as image_dir within classify_images and function 
-#             and as in_arg.dir for function call within main. 
-#            -The results dictionary as results_dic within classify_images 
-#             function and results for the functin call within main.
-#            -The CNN model architecture as model wihtin classify_images function
-#             and in_arg.arch for the function call within main. 
-#           This function uses the extend function to add items to the list 
-#           that's the 'value' of the results dictionary. You will be adding the
-#           classifier label as the item at index 1 of the list and the comparison 
-#           of the pet and classifier labels as the item at index 2 of the list.
-#
-##
-# Imports classifier function for using CNN to classify images 
-from classifier import classifier 
+from classifier import classifier
+import os.path
 
-# TODO 3: Define classify_images function below, specifically replace the None
-#       below by the function definition of the classify_images function. 
-#       Notice that this function doesn't return anything because the 
-#       results_dic dictionary that is passed into the function is a mutable 
-#       data type so no return is needed.
-# 
+
 def classify_images(images_dir, results_dic, model):
     """
     Creates classifier labels with classifier function, compares pet labels to 
@@ -65,20 +38,18 @@ def classify_images(images_dir, results_dic, model):
      Returns:
            None - results_dic is mutable data type so no return needed.         
     """
-    for key in results_dic:
-      
-      model_label = classifier((str(images_dir) + '/' + key), model)
-      model_label = model_label.lower().strip()
-     
-      truth = results_dic[key][0]
+    for key, value in results_dic.items():
+        model_label = classifier(os.path.join(images_dir, key), model)
+        model_label = model_label.lower().strip()
 
-      #if pet labels match classifier labels
-      if truth in model_label: 
-        results_dic[key] = [truth, model_label, 1]
+        truth = value[0]
 
-      #if the labels don't match  
-      else:
-        results_dic[key] = [truth, model_label, 0]
+        #if pet labels match classifier labels
+        if truth in model_label:
+            results_dic[key] = [truth, model_label, 1]
+        #if the labels don't match
+        else:
+            results_dic[key] = [truth, model_label, 0]
 
       
 
