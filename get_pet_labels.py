0afbc07ee72f1552988373376a9455350ae2f808
lhan122 <lhan122@wgu.edu>
2024-06-21 16:01:40 -0500

    In get_pet_labels.py: - removed extra comments - changed for loop on list so it references the file rather then the index - reducing code by using join in list comprehension rather then having another for loop

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -1,28 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
-#                                                                             
-# PROGRAMMER: Leslie Hanson
-# DATE CREATED:  2024-06-20                                
-# REVISED DATE: 
-# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
-#          the image's filename. This function inputs: 
-#           - The Image Folder as image_dir within get_pet_labels function and 
-#             as in_arg.dir for the function call within the main function. 
-#          This function creates and returns the results dictionary as results_dic
-#          within get_pet_labels function and as results within main. 
-#          The results_dic dictionary has a 'key' that's the image filename and
-#          a 'value' that's a list. This list will contain the following item
-#          at index 0 : pet image label (string).
-#
-##
-# Imports python modules
 from os import listdir
 
-# TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
-#       with this function
-# 
+
 def get_pet_labels(image_dir):
     """
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
@@ -40,23 +18,14 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
-    # Replace None with the results_dic dictionary that you created with this
-    # function
+
     filenames = listdir(image_dir)
     results_dic = dict()
 
-    for i in range(len(filenames)):
-      if filenames[i][0] != ".":
-        pet_labels = ''
-        pet_image = str(filenames[i]).lower().split('_')
-  
-        for word in pet_image:
-          if word.isalpha():
-            pet_labels += word + ' '
-        
-        
-        pet_labels = pet_labels.rstrip()
-        results_dic[filenames[i]] = [pet_labels]
-         
+    for file in filenames:
+        if file[0] != ".":
+            pet_image = str(file).lower().split('_')
+            pet_labels = ' '.join([word.strip() for word in pet_image if word.isalpha()])
+        results_dic[file] = [pet_labels]
 
     return results_dic
