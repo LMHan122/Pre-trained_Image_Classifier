8e54d2057c08b36b17cb4db7b19ba8df37a179bf
lhan122 <lhan122@wgu.edu>
2024-06-21 15:09:10 -0500

    In adjust_results4_isadog.py updated dictionary loops to use .values()

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -1,42 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog.py
-#                                                                             
-# PROGRAMMER: Leslie Hanson
-# DATE CREATED: 2024-06-20                                
-# REVISED DATE: 
-# PURPOSE: Create a function adjust_results4_isadog that adjusts the results 
-#          dictionary to indicate whether or not the pet image label is of-a-dog, 
-#          and to indicate whether or not the classifier image label is of-a-dog.
-#          All dog labels from both the pet images and the classifier function
-#          will be found in the dognames.txt file. We recommend reading all the
-#          dog names in dognames.txt into a dictionary where the 'key' is the 
-#          dog name (from dognames.txt) and the 'value' is one. If a label is 
-#          found to exist within this dictionary of dog names then the label 
-#          is of-a-dog, otherwise the label isn't of a dog. Alternatively one 
-#          could also read all the dog names into a list and then if the label
-#          is found to exist within this list - the label is of-a-dog, otherwise
-#          the label isn't of a dog. 
-#         This function inputs:
-#            -The results dictionary as results_dic within adjust_results4_isadog 
-#             function and results for the function call within main.
-#            -The text file with dog names as dogfile within adjust_results4_isadog
-#             function and in_arg.dogfile for the function call within main. 
-#           This function uses the extend function to add items to the list 
-#           that's the 'value' of the results dictionary. You will be adding the
-#           whether or not the pet image label is of-a-dog as the item at index
-#           3 of the list and whether or not the classifier label is of-a-dog as
-#           the item at index 4 of the list. Note we recommend setting the values
-#           at indices 3 & 4 to 1 when the label is of-a-dog and to 0 when the 
-#           label isn't a dog.
-#
-##
-# TODO 4: Define adjust_results4_isadog function below, specifically replace the None
-#       below by the function definition of the adjust_results4_isadog function. 
-#       Notice that this function doesn't return anything because the 
-#       results_dic dictionary that is passed into the function is a mutable 
-#       data type so no return is needed.
-# 
+
 def adjust_results4_isadog(results_dic, dogfile):
     """
     Adjusts the results dictionary to determine if classifier correctly 
@@ -74,27 +38,15 @@ def adjust_results4_isadog(results_dic, dogfile):
     dognames_dict = dict([key.strip(), 1] for key in lines)
 
     #comparing to pet image label
-    for i in results_dic:
-      if results_dic[i][0] in dognames_dict:
-        results_dic[i].append(1)
+    for value in results_dic.values():
+      if value[0] in dognames_dict:
+        value.append(1)
       else:
-        results_dic[i].append(0)
+        value.append(0)
         
     #comparing to classifier label
-    for i in results_dic:
-      if results_dic[i][1] in dognames_dict:
-        results_dic[i].append(1)
+    for value in results_dic.values():
+      if value[1] in dognames_dict:
+        value.append(1)
       else:
-        results_dic[i].append(0)
-    
-    
-
-
-
-
-    
-
-
-
-
-    None
+        value.append(0)
